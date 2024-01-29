270847f8ad98b8a507231235061a32e716b1e675
thomaskovoor <thomaskovoor123@gmail.com>
2024-01-29 21:48:45 +0530

    feat: add JPA Repository and Service class
    add loaddetails service in service class

---
+++ src/main/java/com/example/liveasy/repository/LoadRepository.java
@@ -0,0 +1,7 @@
+package com.example.liveasy.repository;
+
+import com.example.liveasy.entity.LoadEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoadRepository extends JpaRepository<LoadEntity, Long>{
+}
---
+++ src/main/java/com/example/liveasy/service/LoadService.java
@@ -0,0 +1,11 @@
+package com.example.liveasy.service;
+
+import com.example.liveasy.dto.LoadDetailsReq;
+
+public interface LoadService {
+
+    String addLoad(LoadDetailsReq loadDetailsReq);
+
+
+
+}
