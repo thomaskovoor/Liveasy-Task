5513f803a45bb6c380e320ed5260432c63f201db
thomaskovoor <thomaskovoor123@gmail.com>
2024-01-30 14:47:29 +0530

    feat: add findByShipperId function to repository

--- src/main/java/com/example/liveasy/service/LoadService.java
+++ src/main/java/com/example/liveasy/service/LoadService.java
@@ -1,10 +1,13 @@
 package com.example.liveasy.service;
 
 import com.example.liveasy.dto.LoadDetailsDto;
+import com.example.liveasy.dto.LoadListResponse;
+import java.util.UUID;
 
 public interface LoadService {
 
     String addLoad(LoadDetailsDto loadDetailsDto);
+    LoadListResponse getAllLoadsByShipperId(UUID shipperId);
 
 
 
