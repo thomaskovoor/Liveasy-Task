35c94b13c083927ba075ba11d4a277bd2739a81e
thomaskovoor <thomaskovoor123@gmail.com>
2024-01-30 14:52:30 +0530

    feat: Implement getAllLoadsByShipperId function

--- src/main/java/com/example/liveasy/service/impl/LoadServiceImpl.java
+++ src/main/java/com/example/liveasy/service/impl/LoadServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.liveasy.service.impl;
 
 import com.example.liveasy.dto.LoadDetailsDto;
+import com.example.liveasy.dto.LoadListResponse;
 import com.example.liveasy.entity.LoadEntity;
 import com.example.liveasy.repository.LoadRepository;
 import com.example.liveasy.service.LoadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class LoadServiceImpl implements LoadService {
@@ -21,5 +25,39 @@ public class LoadServiceImpl implements LoadService {
         loadRepository.save(loadEntity);
         return "Load details added successfully";
     }
+
+    @Override
+    public LoadListResponse getAllLoadsByShipperId(UUID shipperId) {
+        List<LoadEntity> lst = loadRepository.findAllByShipperId(shipperId);
+
+        if(lst.isEmpty()){
+            return LoadListResponse.builder()
+                    .message("No loads found for the given shipperId")
+                    .loadDetailsDtoList(null)
+                    .build();
+        }
+
+        List<LoadDetailsDto> loadDetailsDtoList = lst.stream()
+                .map(entity -> LoadDetailsDto.builder()
+                        .loadId(entity.getLoadId())
+                        .loadingPoint(entity.getLoadingPoint())
+                        .unloadingPoint(entity.getUnloadingPoint())
+                        .productType(entity.getProductType())
+                        .truckType(entity.getTruckType())
+                        .noOfTrucks(entity.getNoOfTrucks())
+                        .weight(entity.getWeight())
+                        .comment(entity.getComment())
+                        .shipperId(entity.getShipperId())
+                        .date(entity.getDate())
+                        .build())
+                .collect(Collectors.toList());
+
+        return LoadListResponse.builder()
+                .message("All loads fetched successfully")
+                .loadDetailsDtoList(loadDetailsDtoList)
+                .build();
+
+    }
+
 
 }
