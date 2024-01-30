953e32c101f0199fb8806247dca952207af5fb35
thomaskovoor <thomaskovoor123@gmail.com>
2024-01-30 14:51:21 +0530

    feat: add getAllLoadsByShipperId function to service class

--- src/main/java/com/example/liveasy/repository/LoadRepository.java
+++ src/main/java/com/example/liveasy/repository/LoadRepository.java
@@ -3,5 +3,10 @@ package com.example.liveasy.repository;
 import com.example.liveasy.entity.LoadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface LoadRepository extends JpaRepository<LoadEntity, Long>{
+    List<LoadEntity> findAllByShipperId(UUID shipperId);
+
 }
